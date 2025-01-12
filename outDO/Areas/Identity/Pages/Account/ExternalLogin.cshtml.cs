221db5146caca0ec4c7d107d53d33c3905fc55f8
imnofuxkingfun <ralu.exe@gmail.com>
2025-01-12 03:01:10 +0200

    username la google log in

--- outDO/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
+++ outDO/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
@@ -85,8 +85,10 @@ namespace outDO.Areas.Identity.Pages.Account
             [Required]
             [EmailAddress]
             public string Email { get; set; }
+            [Required]
+            public string UserName { get; set; }
         }
-        
+
         public IActionResult OnGet() => RedirectToPage("./Login");
 
         public IActionResult OnPost(string provider, string returnUrl = null)
@@ -132,7 +134,8 @@ namespace outDO.Areas.Identity.Pages.Account
                 {
                     Input = new InputModel
                     {
-                        Email = info.Principal.FindFirstValue(ClaimTypes.Email)
+                        Email = info.Principal.FindFirstValue(ClaimTypes.Email),
+                        UserName = info.Principal.FindFirstValue(ClaimTypes.Name)
                     };
                 }
                 return Page();
@@ -154,7 +157,7 @@ namespace outDO.Areas.Identity.Pages.Account
             {
                 var user = CreateUser();
 
-                await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
+                await _userStore.SetUserNameAsync(user, Input.UserName, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
 
                 var result = await _userManager.CreateAsync(user);
